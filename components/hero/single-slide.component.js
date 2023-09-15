d2a6072405682c2632094de0ac97c96799303932
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-16 00:02:35 +0600

    Create single-slide.component.js

---
+++ components/hero/single-slide.component.js
@@ -0,0 +1,48 @@
+import { Box, Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import BazaarButton from "../ui/button.component";
+
+function SingleSlide({ item }) {
+  console.log(item);
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        height: "100%",
+      }}
+    >
+      <Grid container>
+        <Grid sx={{
+          paddingLeft: "100px",
+          paddingTop: "140px"
+        }} item sm={6}>
+          <Typography>{item.subtitle1}</Typography>
+          <Typography>{item.title}</Typography>
+          <Typography>
+            {item.subtitle2} {item.subtitle3}
+          </Typography>
+          <Typography>{item.subtitle4}</Typography>
+          <BazaarButton />
+        </Grid>
+      </Grid>
+
+      <Box
+        sx={{
+          position: "absolute",
+          right: 0,
+          top: 0,
+        }}
+      >
+        <Image
+          src="/header-poster.png"
+          width={218}
+          height={408}
+          alt="header-img"
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export default SingleSlide;
