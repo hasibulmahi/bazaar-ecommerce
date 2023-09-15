b0cf1650476a7a16c0b40d3cbaede96159b11472
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-16 00:02:14 +0600

    Create navbar.component.js

---
+++ components/header/navbar.component.js
@@ -0,0 +1,388 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+import {
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import Image from "next/image";
+import NavbarRight from "./navbar-right.component";
+
+const drawerWidth = 240;
+// const navItems = ["Home", "Services", "Portfolios", "Blog", "Contact", "..."];
+const navItems = [
+  {
+    text: "Home",
+    permalink: "home",
+    children: [
+      {
+        text: "Home-1",
+        permalink: "home-1",
+      },
+      {
+        text: "Home-1",
+        permalink: "home-1",
+      },
+    ],
+  },
+  {
+    text: "Mega Menu",
+    permalink: "mega-menu",
+    children: [
+      {
+        text: "Home-1",
+        permalink: "home-1",
+      },
+      {
+        text: "Home-1",
+        permalink: "home-1",
+      },
+    ],
+  },
+  {
+    text: "Full Screen Menu",
+    permalink: "full-screen-menu",
+  },
+  {
+    text: "Pages",
+    permalink: "pages",
+  },
+  {
+    text: "User Account",
+    permalink: "user-account",
+  },
+  {
+    text: "Vendor Account",
+    permalink: "vendor-account",
+  },
+];
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Hasibul Mahi
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item, i) => (
+          <ListItem key={i} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }} id={item.permalink}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  const [dropdownEl, setDropDownEl] = React.useState(null);
+  const dropDownOpen = Boolean(dropdownEl);
+  const handleClickDropDown = (event) => {
+    setDropDownEl(event.currentTarget);
+  };
+  const handleCloseDropDown = () => {
+    console.log("clicked");
+    setDropDownEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Container>
+          <Toolbar
+            sx={{
+              px: "0 !important",
+            }}
+          >
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Image src="/bazaar-logo.png" width={100} height={50} alt="Logo" />
+            {/* <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton> */}
+            <Box
+              sx={{
+                display: {
+                  xs: "none",
+                  sm: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                },
+              }}
+            >
+              {navItems.map((item, i) => (
+                <>
+                  <Button
+                    id={`basic-button-${i}`}
+                    sx={{
+                      textTransform: "capitalize",
+                      marginRight: "30px",
+                      color: "#FFF",
+                    }}
+                    key={i}
+                    onClick={handleClickDropDown}
+                    aria-controls={dropDownOpen ? `basic-menu-${i}` : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={dropDownOpen ? "true" : undefined}
+                    // href={`#${item.permalink}`}
+                  >
+                    {item.text}
+                    {item.hasOwnProperty("children") && (
+                      <KeyboardArrowDownIcon />
+                    )}
+                  </Button>
+                  <Menu
+                    id={`basic-menu-${i}`}
+                    anchorEl={dropdownEl}
+                    open={dropDownOpen}
+                    onClose={handleCloseDropDown}
+                    MenuListProps={{
+                      "aria-labelledby": `basic-button-${i}`,
+                    }}
+                  >
+                    <MenuItem onClick={handleCloseDropDown}>Profile</MenuItem>
+                    <MenuItem onClick={handleCloseDropDown}>
+                      My account
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseDropDown}>Logout</MenuItem>
+                  </Menu>
+                </>
+              ))}
+            </Box>
+
+            {/* <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"{}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search> */}
+            <Box sx={{ flexGrow: 1 }} />
+
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
+
+export default Navbar;
