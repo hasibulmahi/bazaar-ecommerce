a2c7af294fa4c07d6129111ea7527f81620bc7d5
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-16 00:01:54 +0600

    updated components

---
+++ components/feature/feature-item.js
@@ -0,0 +1,24 @@
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+
+export const featureItem = [
+  {
+    icon: <LocalShippingIcon />,
+    title: "Fast Delivery",
+    subtitle: "Start from 10$",
+  },
+  {
+    icon: <LocalShippingIcon />,
+    title: "Fast Delivery",
+    subtitle: "Start from 10$",
+  },
+  {
+    icon: <LocalShippingIcon />,
+    title: "Fast Delivery",
+    subtitle: "Start from 10$",
+  },
+  {
+    icon: <LocalShippingIcon />,
+    title: "Fast Delivery",
+    subtitle: "Start from 10$",
+  },
+];
