87dff1b64a6cc244798cbfd79a7f21031db95374
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-16 00:02:23 +0600

    Create header-seeds.js

---
+++ components/hero/header-seeds.js
@@ -0,0 +1,16 @@
+export const headerChild = [
+    {
+        title: "NEW ARRIVALS",
+        subtitle: "SUMMER SALE 20 % OFF",
+        btn: "Shop Now ->",
+        icon: "",
+        image: "/header-shoe.png"
+    },
+    {
+        title: "GAMING 4K",
+        subtitle: "DESKTOP AND LAPTOPS",
+        btn: "Shop Now ->",
+        icon: "",
+        image: "/header-pc.png"  
+    }
+]
