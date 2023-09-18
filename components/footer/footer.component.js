0cdac356a3e6cff24e52b5bf19af7abc38046662
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-19 01:36:37 +0600

    Update footer.component.js

--- components/footer/footer.component.js
+++ components/footer/footer.component.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Footer(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 }
 
 export default Footer;
