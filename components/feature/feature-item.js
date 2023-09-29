9eae9f3b22cf5b958913727900ff44efd64ef315
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-30 02:52:55 +0600

    Update feature-item.js

--- components/feature/feature-item.js
+++ components/feature/feature-item.js
@@ -1,4 +1,7 @@
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import AvTimerOutlinedIcon from '@mui/icons-material/AvTimerOutlined';
+import CreditScoreSharpIcon from '@mui/icons-material/CreditScoreSharp';
 
 export const featureItem = [
   {
@@ -7,18 +10,18 @@ export const featureItem = [
     subtitle: "Start from 10$",
   },
   {
-    icon: <LocalShippingIcon />,
-    title: "Fast Delivery",
-    subtitle: "Start from 10$",
+    icon: <MonetizationOnOutlinedIcon />,
+    title: "Money Guarantee",
+    subtitle: "7 Days Back",
   },
   {
-    icon: <LocalShippingIcon />,
-    title: "Fast Delivery",
-    subtitle: "Start from 10$",
+    icon: <AvTimerOutlinedIcon />,
+    title: "365 Days",
+    subtitle: "For Free Return",
   },
   {
-    icon: <LocalShippingIcon />,
-    title: "Fast Delivery",
-    subtitle: "Start from 10$",
+    icon: <CreditScoreSharpIcon />,
+    title: "Payment",
+    subtitle: "Secure Payment",
   },
 ];
