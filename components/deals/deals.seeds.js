585e02b9c3627e9c9445a6ac007b6b4ee87d08fb
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-29 12:25:41 +0600

    Create deals.seeds.js

---
+++ components/deals/deals.seeds.js
@@ -0,0 +1,39 @@
+import BazaarButton from "../ui/button.component";
+
+export const dealList = [
+  {
+    id: "23436fdgdf5655g",
+    image: "/t-shirt.jpg",
+    title: "Premium T-Shirt",
+    price: "350",
+    totalRating: 3.5,
+  },
+  {
+    id: "23436fdjkif5655g",
+    image: "/t-shirt.jpg",
+    title: "Premium T-Shirt",
+    price: "350",
+    totalRating: 3,
+  },
+  {
+    id: "23436fdgd79j655g",
+    image: "/t-shirt.jpg",
+    title: "Premium T-Shirt",
+    price: "350",
+    totalRating: 4.5,
+  },
+  {
+    id: "23436fdgdf5658ju",
+    image: "/t-shirt.jpg",
+    title: "Premium T-Shirt",
+    price: "350",
+    totalRating: 4,
+  },
+  {
+    id: "23436fdgdf5uim85g",
+    image: "/t-shirt.jpg",
+    title: "Premium T-Shirt",
+    price: "350",
+    totalRating: 4.2,
+  },
+];
