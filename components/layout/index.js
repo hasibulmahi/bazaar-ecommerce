da9768d0c2d27871fba52f31b96456a54b97da28
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-30 02:53:14 +0600

    Update index.js

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import Toolbar from '../header/toolbar.component';
-import Navbar from '../header/navbar.component';
+import React from "react";
+import Toolbar from "../header/toolbar.component";
+import Navbar from "../header/navbar.component";
+import { Box, Container } from "@mui/material";
 
-function Layout({children}) {
-    return (
-        <div>
-            <Toolbar/>
-            <Navbar/>
-            {children}
-        </div>
-    );
+function Layout({ children }) {
+  return (
+    <>
+      <Toolbar />
+      <Navbar />
+      <Box
+        sx={{
+          bgcolor: "#DFDFDF",
+        }}
+      >
+        <Container>{children}</Container>
+      </Box>
+    </>
+  );
 }
 
 export default Layout;
