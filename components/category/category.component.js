d0edf101c61285a4582a231f47f5f25e06e15a17
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-29 12:25:33 +0600

    Create category.component.js

---
+++ components/category/category.component.js
@@ -0,0 +1,26 @@
+import { Box, Button, Container, Grid } from "@mui/material";
+import React from "react";
+import { categoryList } from "./category-seeds";
+import Link from "next/link";
+import Image from "next/image";
+import BazaarButton from "../ui/button.component";
+import CategoryItem from "./category-item";
+
+function Category(props) {
+  return (
+    <Grid container spacing={2}>
+      {categoryList &&
+        categoryList.map((item, i) => (
+          <Grid item sm={2} key={i}>
+            <CategoryItem
+              categoryImage={item.image}
+              categorySlug={item.slug}
+              categoryText={item.text}
+            />
+          </Grid>
+        ))}
+    </Grid>
+  );
+}
+
+export default Category;
