972e28ca55e01052aadbfddcda86f8fcbe26fc5c
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-29 12:25:28 +0600

    Create category-seeds.js

---
+++ components/category/category-seeds.js
@@ -0,0 +1,44 @@
+export const categoryList = [
+  {
+    id: "92342398fdfg9893",
+    title: "Toys",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "toys",
+  },
+  {
+    id: "92342398fdf49893",
+    title: "Sports",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "sports",
+  },
+  {
+    id: "92342398fdfg9893",
+    title: "Gaming",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "gaming",
+  },
+  {
+    id: "92342398fdfg9893",
+    title: "Furniture",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "furniture",
+  },
+  {
+    id: "92342398fdfg9893",
+    title: "Fashion",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "fashion",
+  },
+  {
+    id: "92342398fdfg9893",
+    title: "Cameras",
+    description: "This is toys",
+    image: "/toys.jpg",
+    slug: "cameras",
+  },
+];
