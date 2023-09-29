9213e530bc4ffc8977ae107e2901c956692e6fbb
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-30 02:53:25 +0600

    Update button.component.js

--- components/ui/button.component.js
+++ components/ui/button.component.js
@@ -5,13 +5,21 @@ function BazaarButton({
   text = "Shop now",
   buttonSx,
   buttonVariant = "contained",
+  className,
 }) {
   return (
     <Button
       variant={buttonVariant}
       sx={{
+        backgroundColor: "#2B3445",
+        fontSize: "14px",
+        textTransform: "capitalize",
+        borderRadius: "0px",
+        px: "40px",
+        py: "12px",
         ...buttonSx,
       }}
+      className={className}
     >
       {text}
     </Button>
