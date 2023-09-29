1134623131ebfced63cb2fa6a65925702e0ad7f0
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-29 12:26:08 +0600

    Create hero-item.js

---
+++ components/hero/hero-item.js
@@ -0,0 +1,40 @@
+import BazaarButton from "../ui/button.component";
+
+export const heroContent = [
+  {
+    subtitle1: `LIFESTYLE COLLECTION`,
+    title: `WOMEN`,
+    subtitle2: `SALE UP TO`,
+    subtitle3: `30% OFF`,
+    subtitle4: `Get free Shipping on orders over $99.00`,
+    button: <BazaarButton />,
+    image: "", // add image url here okay? we will use next image.
+  },
+  {
+    subtitle1: `LIFESTYLE COLLECTION`,
+    title: `WOMEN`,
+    subtitle2: `SALE UP TO`,
+    subtitle3: `30% OFF`,
+    subtitle4: `Get free Shipping on orders over $99.00`,
+    button: <BazaarButton />,
+    image: "", // add image url here okay? we will use next image.
+  },
+  {
+    subtitle1: `LIFESTYLE COLLECTION`,
+    title: `WOMEN`,
+    subtitle2: `SALE UP TO`,
+    subtitle3: `30% OFF`,
+    subtitle4: `Get free Shipping on orders over $99.00`,
+    button: <BazaarButton />,
+    image: "", // add image url here okay? we will use next image.
+  },
+  {
+    subtitle1: `LIFESTYLE COLLECTION`,
+    title: `WOMEN`,
+    subtitle2: `SALE UP TO`,
+    subtitle3: `30% OFF`,
+    subtitle4: `Get free Shipping on orders over $99.00`,
+    button: <BazaarButton />,
+    image: "", // add image url here okay? we will use next image.
+  },
+];
