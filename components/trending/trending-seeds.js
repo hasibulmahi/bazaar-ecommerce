a9c657c2b07da5e0529223e795bcb40a3009bac9
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-30 02:53:16 +0600

    Create trending-seeds.js

---
+++ components/trending/trending-seeds.js
@@ -0,0 +1,19 @@
+export const trendingList = [
+    {
+      title: "NEW ARRIVALS",
+      subtitle1: "SUMMER",
+      subtitle2: "SALE 20 % OFF",
+      btn: "Shop Now",
+      icon: "",
+      image: "/header-shoe.png",
+    },
+    {
+      title: "GAMING 4K",
+      subtitle1: "DESKTOP ",
+      subtitle2: "AND LAPTOPS",
+      btn: "Shop Now",
+      icon: "",
+      image: "/header-pc.png",
+    },
+  ];
+  
