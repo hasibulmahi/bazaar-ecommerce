0857ef7e03eca0995997ca0bb5c95a75ca0b727a
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-02 02:50:58 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
+import Category from "@/components/category/category.component";
+import Deals from "@/components/deals/deals.component";
 import Feature from "@/components/feature/feature.component";
 import Hero from "@/components/hero/hero.component";
 import Layout from "@/components/layout";
+import Product from "@/components/product/product.component";
+import Trending from "@/components/trending/trending.component";
 import React from "react";
 
 function Home(props) {
   return (
-    <div>
+    <>
       <Layout>
         <Hero />
         <Feature />
+        <Category />
+        <Deals />
+        <Trending/>
+        <Product/>
       </Layout>
-    </div>
+    </>
   );
 }
 
