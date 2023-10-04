3b8afc67d178193175b81255550408170c68282a
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-04 15:59:04 +0600

    Update trending.component.js

--- components/trending/trending.component.js
+++ components/trending/trending.component.js
@@ -1,53 +1,37 @@
-import React from 'react';
-import { trendingList } from './trending-seeds';
-import Image from 'next/image';
-import { Box, Grid, IconButton, Typography } from '@mui/material';
+import React from "react";
+import { trendingList } from "./trending-seeds";
+import Image from "next/image";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import TrendingCard from "./tending-card";
 
-function Trending(props) {
-    return (
-        <Grid
-        sx={
-          {
-            // paddingLeft: 5,
-          }
-        }
-        item
-        sm={4}
-      >
-        {trendingList &&
-          trendingList.map((item, i) => (
-            <Box
-              key={i}
-              sx={{
-                minHeight: "155px",
-                bgcolor: "#F3EFEC",
-                padding: "0px",
-                position: "relative",
-                marginBottom: 2,
+const trendingCardColor = ["#F4F7FB", "#2B3445", "#EDEBEC"];
+
+function Trending({ tendingSx }) {
+  return (
+    <Grid
+      sx={{
+        display: "flex",
+      }}
+    >
+      {trendingList &&
+        trendingList.map((item, i) => (
+          <Grid item sm={4} key={i}>
+            <TrendingCard
+              title={item.title}
+              subtitle1={item.subtitle1}
+              subtitle2={item.subtitle2}
+              btn={item.btn}
+              cardImage={item.image}
+              tendingSx={{
+                bgcolor: `${trendingCardColor[i]}`,
               }}
-            >
-              <Box >
-              <Typography>{item.title}</Typography>
-              <Typography>{item.subtitle1}</Typography>
-              <Typography>{item.subtitle2}</Typography>
-              <IconButton>{item.btn}</IconButton>
-              </Box>
-
-              <Box
-                sx={{
-                  position: "absolute",
-                  right: 0,
-                  bottom: 0,
-                }}
-              >
-                <Image src={item.image} width={100} height={100} alt="img" />
-              </Box>
-            </Box>
-          ))}
-      </Grid>
+            />
+          </Grid>
+        ))}
+    </Grid>
     //   {/* Grid item start */}
     // </Grid>
-    );
+  );
 }
 
 export default Trending;
