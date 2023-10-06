76d2665e8d686f6877da5fe873d13a35951e8a28
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-07 02:58:44 +0600

    Update product.component.js

--- components/product/product.component.js
+++ components/product/product.component.js
@@ -7,6 +7,9 @@ import {
   CardContent,
   CardMedia,
   Grid,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Typography,
 } from "@mui/material";
 import Rating from "@mui/material/Rating";
@@ -20,88 +23,119 @@ import Link from "next/link";
 function Product(props) {
   const [value, setValue] = useState(2);
   const [activeCategory, setActiveCategory] = useState("92342398fdfg9893");
+  const [categoryProducts, setCategoryProducts] = useState(null);
+  const [categorySlug, setCategorySlug] = useState(null);
 
-  //
+  useEffect(() => {
+    let products = [];
 
-  console.log(activeCategory);
+    productList.map((item, i) => {
+      if (item.category.includes(activeCategory)) {
+        products.push(item);
+      }
+    });
+    setCategoryProducts(products);
+
+    categoryList.map((item, i) => {
+      if (item.id === activeCategory) {
+        setCategorySlug(item.slug);
+      }
+    });
+  }, [activeCategory]);
+
+  console.log(categoryProducts);
+
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item sm={2}>
+        <Grid item sm={2.4}>
           <Box>
             {categoryList &&
               categoryList.map((item, i) => {
                 return (
-                  <Button
-                    variant="text"
-                    onClick={(e) => setActiveCategory(item.id)}
-                  >
-                    {item.title}
-                  </Button>
+                  <ListItem onClick={(e) => setActiveCategory(item.id)}>
+                    <ListItemButton>
+                      <ListItemText primary={item.title} />
+                    </ListItemButton>
+                  </ListItem>
                 );
               })}
           </Box>
         </Grid>
 
-        <Grid item sm={10} >
-          {productList &&
-            productList.map((item, i) => (
-              <Card key={i}
-                sx={{
-                  maxWidth: "220px",
-                  
-                }}
-              >
-                <CardActionArea>
-                  <CardContent
+        <Grid item sm={9.6}>
+          <Grid container spacing={2}>
+            {categoryProducts &&
+              [...categoryProducts].splice(0, 4).map((item, i) => (
+                <Grid item sm={3}>
+                  <Card
+                    key={i}
                     sx={{
-                      textAlign: "center",
+                      maxWidth: "220px",
                     }}
                   >
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <Image
-                        src={item.image}
-                        height={140}
-                        width={120}
-                        alt="img"
+                    <CardActionArea>
+                      <CardContent
+                        sx={{
+                          textAlign: "center",
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <Image
+                            src={item.image}
+                            height={140}
+                            width={120}
+                            alt="img"
+                          />
+                        </Box>
+                        <Typography variant="h6">{item.title}</Typography>
+                        <Typography>${item.price}</Typography>
+                        <Typography>{item.totalRating}</Typography>
+                        <Rating
+                          name="simple-controlled"
+                          value={value}
+                          onChange={(event, newValue) => {
+                            setValue(newValue);
+                          }}
+                          sx={{}}
+                        />
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <BazaarButton
+                        text="Add to cart"
+                        buttonSx={{
+                          bgcolor: "transparent",
+                          border: "1px solid #D8E0E9",
+                          boxShadow: "none",
+                          color: "#4B566B",
+                          position: "relative",
+                          left: "50%",
+                          borderRadius: 1,
+                          transform: "translateX(-50%)",
+                        }}
                       />
-                    </Box>
-                    <Typography variant="h6">{item.title}</Typography>
-                    <Typography>${item.price}</Typography>
-                    <Typography>{item.totalRating}</Typography>
-                    <Rating
-                      name="simple-controlled"
-                      value={value}
-                      onChange={(event, newValue) => {
-                        setValue(newValue);
-                      }}
-                      sx={{}}
-                    />
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <BazaarButton
-                    text="Add to cart"
-                    buttonSx={{
-                      bgcolor: "transparent",
-                      border: "1px solid #D8E0E9",
-                      boxShadow: "none",
-                      color: "#4B566B",
-                      position: "relative",
-                      left: "50%",
-                      borderRadius: 1,
-                      transform: "translateX(-50%)",
-                    }}
-                  />
-                </CardActions>
-              </Card>
-            ))}
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+        </Grid>
+        <Grid item sm={12}>
+          <Box>
+            {/* write your code and logic */}
+            {categoryProducts && categoryProducts.length > 4 && (
+              <Button href={`/category/${categorySlug}`} variant="contained">
+                Show more
+              </Button>
+            )}
+          </Box>
         </Grid>
       </Grid>
     </Box>
