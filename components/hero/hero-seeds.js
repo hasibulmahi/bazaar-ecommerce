5504811e155b63d1ab60337748b3a85aa34629d1
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-07 23:53:57 +0600

    Revert "Update hero-seeds.js"
    This reverts commit 2002102fa9f1c093a3484aef81211186e5c4511c.

--- components/hero/hero-seeds.js
+++ components/hero/hero-seeds.js
@@ -1,18 +1,18 @@
-import EastIcon from "@mui/icons-material/East";
-
 export const heroChild = [
   {
     title: "NEW ARRIVALS",
     subtitle1: "SUMMER",
     subtitle2: "SALE 20 % OFF",
     btn: "Shop Now",
+    icon: "",
     image: "/header-shoe.png",
   },
   {
     title: "GAMING 4K",
-    subtitle1: "DESKTOPS & ",
-    subtitle2: "LAPTOPS",
+    subtitle1: "DESKTOP ",
+    subtitle2: "AND LAPTOPS",
     btn: "Shop Now",
+    icon: "",
     image: "/header-pc.png",
   },
 ];
