72d03a843f167d9a9ff67bcf043a49c69b8f5508
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-11 23:23:59 +0600

    Update hero.component.js

--- components/hero/hero.component.js
+++ components/hero/hero.component.js
@@ -42,7 +42,7 @@ function Hero(props) {
       </div>
     ),
     customPaging: (i) => (
-      <div
+      <Box
         style={{
           width: "30px",
           color: "blue",
@@ -55,7 +55,7 @@ function Hero(props) {
         >
           <FiberManualRecordIcon />
         </Button>
-      </div>
+      </Box>
     ),
   };
   return (
