054be42aee5d24f0be4ac23aea3dea15f2f07fcb
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-12 01:39:56 +0600

    Update deals.component.js

--- components/deals/deals.component.js
+++ components/deals/deals.component.js
@@ -7,6 +7,7 @@ import {
   CardContent,
   CardMedia,
   Grid,
+  IconButton,
   Typography,
 } from "@mui/material";
 import Rating from "@mui/material/Rating";
@@ -14,24 +15,58 @@ import React, { useState } from "react";
 import { dealList } from "./deals.seeds";
 import Image from "next/image";
 import BazaarButton from "../ui/button.component";
+import EastIcon from "@mui/icons-material/East";
 
 function Deals(props) {
   const [value, setValue] = useState(2);
   return (
-    <Box>
+    <Box
+      sx={{
+        paddingTop: 5,
+        color: "#2B3445",
+      }}
+    >
       <Box
         sx={{
           display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: 1,
         }}
       >
-        <Typography>Deals of the day</Typography>
         <Typography
           sx={{
+            color: "#2B3445",
+          }}
+          fontSize={15}
+          fontWeight={600}
+        >
+          Deals of the day
+        </Typography>
+        <Box
+          sx={{
             marginLeft: "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          More Products
-        </Typography>
+          <Typography
+            sx={{
+              fontSize: "14px",
+            }}
+          >
+            More Products
+          </Typography>
+          <IconButton>
+            <EastIcon
+              sx={{
+                color: "#2B3445",
+              }}
+              fontSize="small"
+            />
+          </IconButton>
+        </Box>
       </Box>
       <Grid container spacing={2}>
         {dealList &&
@@ -62,17 +97,36 @@ function Deals(props) {
                       textAlign: "center",
                     }}
                   >
-                    <Typography variant="h6">{item.title}</Typography>
-                    <Typography>${item.price}</Typography>
-                    <Typography>{item.totalRating}</Typography>
-                    <Rating
-                      name="simple-controlled"
-                      value={value}
-                      onChange={(event, newValue) => {
-                        setValue(newValue);
+                    <Typography fontSize={14}>{item.title}</Typography>
+                    <Typography fontSize={14} fontWeight="600">
+                      ${item.price}
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
                       }}
-                      sx={{}}
-                    />
+                    >
+                      <Rating
+                        name="simple-controlled"
+                        size="small"
+                        value={value}
+                        onChange={(event, newValue) => {
+                          setValue(newValue);
+                        }}
+                        sx={{}}
+                      />
+                      <Typography
+                        sx={{
+                          paddingLeft: 1,
+                          fontSize: "14px",
+                          color: "#7D879C",
+                        }}
+                      >
+                        {item.totalRating}
+                      </Typography>
+                    </Box>
                   </CardContent>
                 </CardActionArea>
                 <CardActions>
@@ -87,6 +141,7 @@ function Deals(props) {
                       left: "50%",
                       borderRadius: 1,
                       transform: "translateX(-50%)",
+                      py: "2px"
                     }}
                   />
                 </CardActions>
