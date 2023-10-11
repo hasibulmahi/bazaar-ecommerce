02f98e8c9b67c0b03fe0bf18896cffe1b18638a2
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-11 23:23:56 +0600

    Update tending-card.js

--- components/trending/tending-card.js
+++ components/trending/tending-card.js
@@ -1,6 +1,7 @@
 import { Box, IconButton, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
+import EastIcon from "@mui/icons-material/East";
 
 function TrendingCard({
   title,
@@ -14,7 +15,7 @@ function TrendingCard({
     <Box
       sx={{
         minHeight: "155px",
-        minWidth: "380px",
+        minWidth: "350px",
         bgcolor: "#F3EFEC",
         padding: "0px",
         position: "relative",
@@ -22,15 +23,53 @@ function TrendingCard({
         display: "flex",
         alignItems: "center",
         justifyContent: "start",
+        mt: 5,
         // bgcolor: `${trendingCardColor[i]}`,
         ...tendingSx,
       }}
     >
-      <Box>
-        <Typography>{title}</Typography>
-        <Typography>{subtitle1}</Typography>
+      <Box
+        sx={{
+          ml: 4,
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "15px",
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "20px",
+          }}
+        >
+          {subtitle1}
+        </Typography>
         <Typography>{subtitle2}</Typography>
-        <IconButton>{btn}</IconButton>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "15px",
+            }}
+          >
+            {btn}
+          </Typography>
+          <IconButton>
+            <EastIcon
+              sx={{
+                color: "#2B3445",
+              }}
+              fontSize="small"
+            />
+          </IconButton>
+        </Box>
       </Box>
 
       <Box
