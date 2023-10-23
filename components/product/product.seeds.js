8d53a4764edbf833f97233fab9174887774a8d83
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-23 20:01:44 +0600

    Update product.seeds.js

--- components/product/product.seeds.js
+++ components/product/product.seeds.js
@@ -1,32 +1,40 @@
 export const productList = [
   {
-    id: "23436fdjkif5655g",
+    id: "23436fdjku7mnf5655g",
     image: "/t-shirt.jpg",
     title: "Premium T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 3,
     category: ["92342398fdfg9893"],
   },
   {
-    id: "23436fdjkif5655g",
+    id: "23436fdjkio9025655g",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Low T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 3,
     category: ["92342398fdfg9893"],
   },
   {
-    id: "23436fdjkif5655g",
+    id: "2343hhlknjkif5655g",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Hight T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 3,
     category: ["92342398fdfg9893"],
   },
   {
-    id: "23436fdjkif5655g",
+    id: "23436fiod465kif5655g",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Super High T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 3,
     category: ["92342398fdfg9893"],
@@ -34,23 +42,29 @@ export const productList = [
   {
     id: "23436fdgd79j655g",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Normal T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4.5,
     category: ["923423983dfg9893"],
   },
   {
-    id: "23436fdgdf5658ju",
+    id: "23436fd8iuhdf5658ju",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Pro T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4,
     category: ["9234239jfdfg9893"],
   },
   {
-    id: "23436fdgdf5uim85g",
+    id: "23436fdgdfo89uim85g",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Pro max T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4.2,
     category: ["9234239ofdfg9893"],
@@ -58,15 +72,19 @@ export const productList = [
   {
     id: "92342398fdf49893",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "ultra T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4.5,
     category: ["923423983dfg9893"],
   },
   {
-    id: "23436fdgdf5658ju",
+    id: "8909j55jjkjjhghjb",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "Hudai T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4,
     category: ["9234239jfdfg9893"],
@@ -74,7 +92,9 @@ export const productList = [
   {
     id: "23436fdgd875658ju",
     image: "/t-shirt.jpg",
-    title: "Premium T-Shirt",
+    title: "erf T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4,
     category: ["9234239jfdfg9893"],
@@ -83,6 +103,8 @@ export const productList = [
     id: "23436fdg6yf5658ju",
     image: "/t-shirt.jpg",
     title: "Premium T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4,
     category: ["9234239jfdfg9893"],
@@ -91,6 +113,8 @@ export const productList = [
     id: "23436fdg8ukyf5658ju",
     image: "/t-shirt.jpg",
     title: "Premium T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4,
     category: ["9234239jfdfg9893"],
@@ -99,6 +123,8 @@ export const productList = [
     id: "23436fdgdf5uim85g",
     image: "/t-shirt.jpg",
     title: "Premium T-Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius massa id ut mattis. Facilisis vitae gravida egestas ac account.",
     price: "350",
     totalRating: 4.2,
     category: ["9234239ofdfg9893"],
