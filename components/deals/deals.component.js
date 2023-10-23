9734d2d25f35310b07c6cb0101aa5686d79fc979
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-23 20:01:26 +0600

    Update deals.component.js

--- components/deals/deals.component.js
+++ components/deals/deals.component.js
@@ -15,143 +15,156 @@ import { dealList } from "./deals.seeds";
 import Image from "next/image";
 import BazaarButton from "../ui/button.component";
 import EastIcon from "@mui/icons-material/East";
+import CartCard from "../cart-card/cart-card.component";
 
 function Deals(props) {
   const [value, setValue] = useState(2);
+  const [openCart, setOpenCart] = useState(null);
+  const handleCloseCart = () => {
+    setOpenCart(null);
+  };
 
   return (
-    <Box
-      sx={{
-        paddingTop: 5,
-        color: "#2B3445",
-      }}
-    >
+    <>
+      <CartCard
+        openCart={openCart ?? false}
+        item={openCart}
+        handleCloseCart={handleCloseCart}
+      />
       <Box
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: 1,
+          paddingTop: 5,
+          color: "#2B3445",
         }}
       >
-        <Typography
-          sx={{
-            color: "#2B3445",
-          }}
-          fontSize={15}
-          fontWeight={600}
-        >
-          Deals of the day
-        </Typography>
         <Box
           sx={{
-            marginLeft: "auto",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
+            marginBottom: 1,
           }}
         >
           <Typography
             sx={{
-              fontSize: "14px",
+              color: "#2B3445",
+            }}
+            fontSize={15}
+            fontWeight={600}
+          >
+            Deals of the day
+          </Typography>
+          <Box
+            sx={{
+              marginLeft: "auto",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            More Products
-          </Typography>
-          <IconButton>
-            <EastIcon
+            <Typography
               sx={{
-                color: "#2B3445",
+                fontSize: "14px",
               }}
-              fontSize="small"
-            />
-          </IconButton>
-        </Box>
-      </Box>
-      <Grid container spacing={2}>
-        {dealList &&
-          dealList.map((item, i) => (
-            <Grid item sm={2.4} key={i}>
-              <Card
+            >
+              More Products
+            </Typography>
+            <IconButton>
+              <EastIcon
                 sx={{
-                  maxWidth: "220px",
-                  py: 2,
+                  color: "#2B3445",
                 }}
-              >
-                <CardActionArea>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Image
-                      src={item.image}
-                      height={140}
-                      width={120}
-                      alt="img"
-                    />
-                  </Box>
-                  <CardContent
-                    sx={{
-                      textAlign: "center",
-                    }}
-                  >
-                    <Typography fontSize={14}>{item.title}</Typography>
-                    <Typography fontSize={14} fontWeight="600">
-                      ${item.price}
-                    </Typography>
-                    <Box
+                fontSize="small"
+              />
+            </IconButton>
+          </Box>
+        </Box>
+        <Grid container spacing={2}>
+          {dealList &&
+            dealList.map((item, i) => (
+              <Grid item sm={2.4} key={i}>
+                <Card
+                  sx={{
+                    maxWidth: "220px",
+               
+                  }}
+                >
+                  <CardActionArea>
+                    <CardContent
+                      onClick={() => setOpenCart(item)}
                       sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
+                        textAlign: "center",
                       }}
                     >
-                      <Rating
-                        name="simple-controlled"
-                        size="small"
-                        value={value}
-                        onChange={(event, newValue) => {
-                          setValue(newValue);
-                        }}
-                        sx={{}}
-                      />
-                      <Typography
+                      <Box
                         sx={{
-                          paddingLeft: 1,
-                          fontSize: "14px",
-                          color: "#7D879C",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
                         }}
                       >
-                        {item.totalRating}
+                        <Image
+                          src={item.image}
+                          height={140}
+                          width={120}
+                          alt="img"
+                        />
+                      </Box>
+                      <Typography fontSize={14}>{item.title}</Typography>
+                      <Typography fontSize={14} fontWeight="600">
+                        ${item.price}
                       </Typography>
-                    </Box>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <BazaarButton
-                    item={item}
-                    text="Add to cart df"
-                    buttonSx={{
-                      bgcolor: "transparent",
-                      border: "1px solid #D8E0E9",
-                      boxShadow: "none",
-                      color: "#4B566B",
-                      position: "relative",
-                      left: "50%",
-                      borderRadius: 1,
-                      transform: "translateX(-50%)",
-                      py: "2px",
-                    }}
-                  />
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-      </Grid>
-    </Box>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <Rating
+                          name="simple-controlled"
+                          size="small"
+                          value={value}
+                          onChange={(event, newValue) => {
+                            setValue(newValue);
+                          }}
+                          sx={{}}
+                        />
+                        <Typography
+                          sx={{
+                            paddingLeft: 1,
+                            fontSize: "14px",
+                            color: "#7D879C",
+                          }}
+                        >
+                          {item.totalRating}
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <BazaarButton
+                      item={item}
+                      text="Add to cart df"
+                      buttonSx={{
+                        bgcolor: "transparent",
+                        border: "1px solid #D8E0E9",
+                        boxShadow: "none",
+                        color: "#4B566B",
+                        position: "relative",
+                        left: "50%",
+                        borderRadius: 1,
+                        transform: "translateX(-50%)",
+                        py: "2px",
+                      }}
+                    />
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+    </>
   );
 }
 
