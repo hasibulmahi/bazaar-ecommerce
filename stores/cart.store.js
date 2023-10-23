d941b0e96534dd1aad57b0ec99ee80742f3331fc
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-23 12:20:32 +0600

    Update cart.store.js

--- stores/cart.store.js
+++ stores/cart.store.js
@@ -1,18 +1,69 @@
 import { create } from "zustand";
 
-// export const useCartStore = create((set) => ({
-//   cartProducts: [],
-//   setCartProducts: (c) =>
-//     set((state) => ({ cartProducts: state.cartProducts.push(c) })),
-//   //   removeAllCard: () => set({ cartProducts: [] }),
-// }));
+const removeOneItem = (products, item) => {
+  // amader j item ache tar kono duplicate ache ki na
+  // joodi duplicate thake  - tahoel sei gulo k 1t array te nibo
+  // tarpot oi array theke first item ta bad dibo
+  // r jodi duplicate item na thake tahole oi item bade baki gulo k niye nibo
+  // const filteredItem = products.filter((item)=>)
+};
 
-const useBearStore = create((set) => ({
+export const addCartProducts = (products, p) => {
+  console.log("products", products.length);
+  console.log("p", p);
+  let cartProducts = [];
+  if (products.length >= 1 && products.length <= 10) {
+    products.map((item) => {
+      if (item.id === p.id) {
+        cartProducts.push({
+          id: item.id,
+          title: item.title,
+          image: item.image,
+          price: item.price,
+          quantity: item.quantity + 1,
+        });
+      } else {
+        cartProducts.push({
+          id: p.id,
+          title: p.title,
+          image: p.image,
+          price: p.price,
+          quantity: 1,
+        });
+      }
+    });
+  }
+  if (products.length === 0) {
+    console.log("salam");
+    cartProducts.push({
+      id: p.id,
+      title: p.title,
+      image: p.image,
+      price: p.price,
+      quantity: 1,
+    });
+  }
+  console.log("cartProducts==", cartProducts);
+  return cartProducts;
+};
+
+export const useCartStore = create((set) => ({
+  cartProducts: [],
+  // setCartProducts: (p) =>
+  //   set((state) => ({ cartProducts: [p, ...state.cartProducts] })),
+  setCartProducts: (p) =>
+    set((state) => ({
+      cartProducts: [...addCartProducts(state.cartProducts, p)],
+    })),
+  removeCartProducts: (p) =>
+    set((state) => ({
+      cartProducts: [...state.cartProducts.filter((item) => item.id !== p.id)],
+    })),
+  removeAllCart: () => set((state) => ({ cartProducts: [] })),
+}));
+
+export const useBearStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
 }));
-
-export const myFunction = () => {
-  return "Hello world";
-};
