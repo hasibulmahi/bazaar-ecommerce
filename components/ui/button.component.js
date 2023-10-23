9de4661a2ab52e57444d1e90e05d7dc4bdda980e
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-23 12:20:14 +0600

    Update button.component.js

--- components/ui/button.component.js
+++ components/ui/button.component.js
@@ -1,4 +1,4 @@
-import { myFunction, useBearStore } from "@/stores/cart.store";
+import { useCartStore } from "@/stores/cart.store";
 import { Button, IconButton } from "@mui/material";
 import React from "react";
 
@@ -7,25 +7,31 @@ function BazaarButton({
   buttonSx,
   buttonVariant = "contained",
   className,
+  item = {},
 }) {
-  console.log(myFunction());
-  // const { cartProducts, setCartProducts } = useCartStore();
+  const { cartProducts, setCartProducts, removeCartProducts } = useCartStore();
+  console.log("cartProducts", cartProducts);
   return (
-    <Button
-      variant={buttonVariant}
-      sx={{
-        backgroundColor: "#2B3445",
-        fontSize: "14px",
-        textTransform: "capitalize",
-        borderRadius: "0px",
-        px: "40px",
-        py: "12px",
-        ...buttonSx,
-      }}
-      className={className}
-    >
-      {text}
-    </Button>
+    <>
+      <Button
+        onClick={() => setCartProducts(item)}
+        variant={buttonVariant}
+        sx={{
+          backgroundColor: "#2B3445",
+          fontSize: "14px",
+          textTransform: "capitalize",
+          borderRadius: "0px",
+          px: "40px",
+          py: "12px",
+          ...buttonSx,
+        }}
+        className={className}
+      >
+        {text}
+      </Button>
+      <br />
+      <Button onClick={() => removeCartProducts(item)}>remove</Button>
+    </>
   );
 }
 
