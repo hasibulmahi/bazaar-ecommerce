f696bdd02287b92d51fd17b8be45eef9270f14a5
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-25 19:41:20 +0600

    Update cart.store.js

--- stores/cart.store.js
+++ stores/cart.store.js
@@ -28,6 +28,9 @@ export const addCartProducts = (products, p) => {
   } else {
     cartProducts.push(cartItemObject(p));
   }
+  typeof window !== "undefined" &&
+    window.localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+
   return cartProducts;
 };
 export const removeProducts = (products, p) => {
@@ -39,11 +42,43 @@ export const removeProducts = (products, p) => {
   } else {
     cartProducts = cartProducts.filter((item) => item.id !== p.id);
   }
+
+  typeof window !== "undefined" &&
+    window.localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+
+  return cartProducts;
+};
+
+const cartProducts = () => {
+  const getLocalStorageCartProduct =
+    typeof window !== "undefined" &&
+    window.localStorage.getItem("cartProducts");
+
+  if (!isEmpty(getLocalStorageCartProduct)) {
+    return JSON.parse(getLocalStorageCartProduct);
+  } else {
+    return [];
+  }
+};
+
+//state.cartProducts.filter((item) => item.id !== p.id)
+
+const removeSameProducts = (products, p) => {
+  let cartProducts = [...products];
+  const removedItems = products.filter((item) => item.id !== p.id);
+  console.log("removedItems", removedItems);
+  if (removedItems.length) {
+    cartProducts = [...removedItems];
+  } else {
+    cartProducts = [];
+  }
+  typeof window !== "undefined" &&
+    window.localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
   return cartProducts;
 };
 
 export const useCartStore = create((set) => ({
-  cartProducts: [],
+  cartProducts: cartProducts(),
   // setCartProducts: (p) =>
   //   set((state) => ({ cartProducts: [p, ...state.cartProducts] })),
   setCartProducts: (p) =>
@@ -54,6 +89,10 @@ export const useCartStore = create((set) => ({
     set((state) => ({
       cartProducts: [...removeProducts(state.cartProducts, p)],
     })),
+  removeSameCartProducts: (p) =>
+    set((state) => ({
+      cartProducts: [...removeSameProducts(state.cartProducts, p)],
+    })),
   removeAllCart: () => set((state) => ({ cartProducts: [] })),
 }));
 
@@ -62,3 +101,22 @@ export const useBearStore = create((set) => ({
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
 }));
+
+// first a check korte hobe localstorage a kono object ache ki na - particular name a
+// ebar check korte hobe zustand er kache cart er product ache ki na
+// ebar jodi local strorage a thake seita zustand a setCart diye set korte hobe
+
+// zustand a jookhon kono item add or remove hobe seita local storage a set korte hobe
+
+// if (!isEmpty(getLocalStorageCartProduct)) {
+//   localStorage.setItem("cartProducts", useCartStore.cartProducts);
+// }
+
+const getLocalStorageCartProduct =
+  typeof window !== "undefined" && window.localStorage.getItem("cartProducts");
+// if (!isEmpty(getLocalStorageCartProduct)) {
+//   console.log("I am inside this");
+//   // window.localStorage.setItem("cartProducts");
+//   console.log("useCartStore", useCartStore());
+// }
+console.log("getLocalStorageCartProduct", getLocalStorageCartProduct);
