f9c835aea4614800899f3bc95f9e3ddf9c1c4519
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-01 00:43:18 +0600

    Update navbar.component.js

--- components/header/navbar.component.js
+++ components/header/navbar.component.js
@@ -85,45 +85,6 @@ const navItems = [
   },
 ];
 
-// const Search = styled("div")(({ theme }) => ({
-//   position: "relative",
-//   borderRadius: theme.shape.borderRadius,
-//   backgroundColor: alpha(theme.palette.common.white, 0.15),
-//   "&:hover": {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
-//   },
-//   marginRight: theme.spacing(2),
-//   marginLeft: 0,
-//   width: "100%",
-//   [theme.breakpoints.up("sm")]: {
-//     marginLeft: theme.spacing(3),
-//     width: "auto",
-//   },
-// }));
-
-// const SearchIconWrapper = styled("div")(({ theme }) => ({
-//   padding: theme.spacing(0, 2),
-//   height: "100%",
-//   position: "absolute",
-//   pointerEvents: "none",
-//   display: "flex",
-//   alignItems: "center",
-//   justifyContent: "center",
-// }));
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: "inherit",
-//   "& .MuiInputBase-input": {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create("width"),
-//     width: "100%",
-//     [theme.breakpoints.up("md")]: {
-//       width: "20ch",
-//     },
-//   },
-// }));
 
 function Navbar(props) {
   const { window } = props;
@@ -156,7 +117,7 @@ function Navbar(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  // const isMenuOpen = Boolean(anchorEl);
+
   // const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   // const handleProfileMenuOpen = (event) => {
@@ -252,9 +213,7 @@ function Navbar(props) {
 
   const [dropdownEl, setDropDownEl] = React.useState(null);
   const dropDownOpen = Boolean(dropdownEl);
-  // const handleClickDropDown = (event) => {
-  //   setDropDownEl(event.currentTarget);
-  // };
+
   const handleCloseDropDown = () => {
     setDropDownEl(null);
   };
