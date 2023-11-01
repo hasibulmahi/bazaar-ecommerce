8c7cadb2e1e473ddb5db433f3595b51193640329
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-02 02:19:25 +0600

    Update navbar.component.js

--- components/header/navbar.component.js
+++ components/header/navbar.component.js
@@ -6,8 +6,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
