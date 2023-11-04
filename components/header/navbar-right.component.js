b5affc6eae93852804418aecb3f845ffd77564d7
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-05 00:40:49 +0600

    Update navbar-right.component.js

--- components/header/navbar-right.component.js
+++ components/header/navbar-right.component.js
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 
 import Box from "@mui/material/Box";
-
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 import AccountCircle from "@mui/icons-material/AccountCircle";
