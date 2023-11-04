e42777361ee4de01b36389b6d2961c04f75f7904
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-05 00:37:40 +0600

    Update navbar-right.component.js

--- components/header/navbar-right.component.js
+++ components/header/navbar-right.component.js
@@ -126,13 +126,7 @@ function NavbarRight(props) {
           />
         </IconButton>
         <IconButton
-        // size="large"
-        // edge="end"
-        // aria-label="account of current user"
-        // aria-controls={menuId}
-        // aria-haspopup="true"
-        // onClick={handleProfileMenuOpen}
-        // color="inherit"
+        
         >
           <AccountCircle
             sx={{
