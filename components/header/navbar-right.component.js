2242af2a8e56cf381d1ece94645e8217103f2ae8
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-05 00:35:13 +0600

    Update navbar-right.component.js

--- components/header/navbar-right.component.js
+++ components/header/navbar-right.component.js
@@ -14,9 +14,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 
 import {
