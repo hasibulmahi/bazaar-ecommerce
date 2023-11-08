54fafa66cd816069304c9b1da0f93c22b2254a24
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-08 21:41:17 +0600

    Update feature.component.js

--- components/feature/feature.component.js
+++ components/feature/feature.component.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { featureItem } from "./feature-item";
-import { Box, Container, Grid, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
 
 function Feature(props) {
+  const [mouseEnter, setMouseEnter] = useState(null);
   return (
     <Container
       sx={{
@@ -16,29 +24,51 @@ function Feature(props) {
           featureItem.map((item, i) => (
             <Grid
               item
-              sm={3}
+              lg={3}
+              sm={6}
+              xs={12}
               key={i}
               sx={{
-                display: "flex",
                 px: 3,
-                py: 2,
+                py: 3,
               }}
             >
-              <IconButton
+              <Box
+                onMouseEnter={() => setMouseEnter(i)}
+                onMouseLeave={() => setMouseEnter(null)}
                 sx={{
-                  mr: 2,
-                  color: "#2B3445",
+                  display: "flex",
+                  width: "100%",
+                  borderRight:
+                    i !== featureItem.length - 1 ? "1px solid #eee" : "0px",
                 }}
               >
-                {item.icon}
-              </IconButton>
-              <Box>
-                <Typography color="#2B3445" fontSize={15} fontWeight="bold">
-                  {item.title}
-                </Typography>
-                <Typography color="#7D879C" fontSize={14}>
-                  {item.subtitle}
-                </Typography>
+                <IconButton
+                  sx={{
+                    mr: 2,
+                    color: "#2B3445",
+                    transition: ".30s linear",
+                    transform: `scale(${mouseEnter === i ? 1.2 : 1})`,
+                  }}
+                >
+                  {item.icon}
+                </IconButton>
+                <Box>
+                  <Typography
+                    color="#2B3445"
+                    fontSize={15}
+                    fontWeight={600}
+                    sx={{
+                      transition: ".30s linear",
+                      letterSpacing: `${mouseEnter === i ? "1px" : ".30px"}`,
+                    }}
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography color="#7D879C" fontSize={14}>
+                    {item.subtitle}
+                  </Typography>
+                </Box>
               </Box>
             </Grid>
           ))}
