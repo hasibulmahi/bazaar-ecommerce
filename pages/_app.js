b9075e516e8d83dd8cb533878f834a075f9bda1e
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-08 21:41:10 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { ThemeOptions } from "@/theme";
+
+import { ThemeProvider, createTheme } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={ThemeOptions}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
