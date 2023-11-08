1f3a14b7aed909128bd9a1b166e9dde4f1757dd0
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-08 21:41:23 +0600

    Update feature-item.js

--- components/feature/feature-item.js
+++ components/feature/feature-item.js
@@ -1,26 +1,50 @@
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
-import AvTimerOutlinedIcon from '@mui/icons-material/AvTimerOutlined';
-import CreditScoreSharpIcon from '@mui/icons-material/CreditScoreSharp';
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AvTimerOutlinedIcon from "@mui/icons-material/AvTimerOutlined";
+import CreditScoreSharpIcon from "@mui/icons-material/CreditScoreSharp";
 
 export const featureItem = [
   {
-    icon: <LocalShippingIcon />,
+    icon: (
+      <LocalShippingIcon
+        sx={{
+          fontSize: "36px",
+        }}
+      />
+    ),
     title: "Fast Delivery",
     subtitle: "Start from 10$",
   },
   {
-    icon: <MonetizationOnOutlinedIcon />,
+    icon: (
+      <MonetizationOnOutlinedIcon
+        sx={{
+          fontSize: "36px",
+        }}
+      />
+    ),
     title: "Money Guarantee",
     subtitle: "7 Days Back",
   },
   {
-    icon: <AvTimerOutlinedIcon />,
+    icon: (
+      <AvTimerOutlinedIcon
+        sx={{
+          fontSize: "36px",
+        }}
+      />
+    ),
     title: "365 Days",
     subtitle: "For Free Return",
   },
   {
-    icon: <CreditScoreSharpIcon />,
+    icon: (
+      <CreditScoreSharpIcon
+        sx={{
+          fontSize: "36px",
+        }}
+      />
+    ),
     title: "Payment",
     subtitle: "Secure Payment",
   },
