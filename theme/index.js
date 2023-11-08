11eddd7966d38fa607de8ccdbfb295b2f054738c
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-08 21:41:03 +0600

    Create index.js

---
+++ theme/index.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const ThemeOptions = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    fontFamily: "Open Sans",
+  },
+});
