e21cdb69505e6823e2bc3c5e7ef4046fb8ec3d97
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-12 01:29:38 +0600

    Update product.component.js

--- components/product/product.component.js
+++ components/product/product.component.js
@@ -71,6 +71,7 @@ function Product(props) {
     dots: true,
     infinite: true,
     speed: 500,
+    initialSlide: 0,
     slidesToShow:
       categoryProducts?.length >= 4
         ? 4
@@ -84,32 +85,32 @@ function Product(props) {
         ? categoryProducts?.length
         : 4,
 
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 0,
+    //       slidesToScroll: 0,
+    //       initialSlide: 0,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 0,
+    //       slidesToScroll: 0,
+    //     },
+    //   },
+    // ],
   };
   useEffect(() => {
     let products = [];
