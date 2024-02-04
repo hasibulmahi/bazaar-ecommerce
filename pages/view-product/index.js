47d79414bf220dc6e5f3df99a6c1ea86ff855133
Hasibul Mahi <hasibulmahi@gmail.com>
2024-02-04 12:46:45 +0600

    Update index.js

--- pages/view-product/index.js
+++ pages/view-product/index.js
@@ -26,16 +26,18 @@ function Home(props) {
             </Grid>
             <Grid item sm={6}>
               <Box>
-                <Typography variant="h4" fontWeight={600} >Premium T-Shirt</Typography>
-                <Typography>Brand: Xiaomi</Typography>
-                <Typography>Rating: ****** (50)</Typography>
-                <Box>
+                <Typography variant="h4" fontWeight={600} mb={2} >Premium T-Shirt</Typography>
+                <Typography mb={1}>Brand: Xiaomi</Typography>
+                <Typography mb={1}>Rating: ****** (50)</Typography>
+                <Box mb={1}>
                   <Button variant="outlined">Option 1</Button>
                   <Button variant="outlined">Option 1</Button>
                 </Box>
-                <Typography>Price: $244.00</Typography>
-                <Typography>Stock Available</Typography>
-                <Button variant="contained">Add To Cart</Button>
+                <Typography mb={1}>Price: $244.00</Typography>
+                <Typography mb={1}>Stock Available</Typography>
+                <Button sx={{
+                  mb: 1
+                }} variant="contained">Add To Cart</Button>
                 <Typography>Sold By: Mobile Store</Typography>
               </Box>
             </Grid>
