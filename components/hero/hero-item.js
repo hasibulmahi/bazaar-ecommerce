669f1186842a098d6c1052f1d6da8264714c6717
Hasibul Mahi <hasibulmahi@gmail.com>
2024-02-25 19:06:25 +0600

    Update hero-item.js

--- components/hero/hero-item.js
+++ components/hero/hero-item.js
@@ -10,15 +10,15 @@ export const heroContent = [
   //   button: <BazaarButton />,
   //   image: "", // add image url here okay? we will use next image.
   // },
-  {
-    subtitle1: `LIFESTYLE COLLECTION`,
-    title: `WOMEN`,
-    subtitle2: `SALE UP TO`,
-    subtitle3: `30% OFF`,
-    subtitle4: `Get free Shipping on orders over $99.00`,
-    button: <BazaarButton />,
-    image: "", // add image url here okay? we will use next image.
-  },
+  // {
+  //   subtitle1: `LIFESTYLE COLLECTION`,
+  //   title: `WOMEN`,
+  //   subtitle2: `SALE UP TO`,
+  //   subtitle3: `30% OFF`,
+  //   subtitle4: `Get free Shipping on orders over $99.00`,
+  //   button: <BazaarButton />,
+  //   image: "", // add image url here okay? we will use next image.
+  // },
   {
     subtitle1: `LIFESTYLE COLLECTION`,
     title: `WOMEN`,
