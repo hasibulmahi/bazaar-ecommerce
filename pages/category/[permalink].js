1cf3b7987ee291565939136a4e0c163e34fed2cb
Hasibul Mahi <hasibulmahi@gmail.com>
2024-02-27 01:33:19 +0600

    Update [permalink].js

--- pages/category/[permalink].js
+++ pages/category/[permalink].js
@@ -1,6 +1,6 @@
 import { categoryList } from "@/components/category/category-seeds";
 import { useRouter } from "next/router";
-// import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 
 function CategoryPage(props) {
   const router = useRouter();
