4e493609f7850823b589873eb553ec9a38cc7e47
Hasibul Mahi <hasibulmahi@gmail.com>
2024-02-29 23:51:04 +0600

    Update product.component copy.js

--- components/product/product.component copy.js
+++ components/product/product.component copy.js
@@ -83,20 +83,20 @@ function Product(props) {
     //       dots: true,
     //     },
     //   },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-    //   },
     //   {
-    //     breakpoint: 480,
+    //     breakpoint: 600,
     //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //       initialSlide: 2,
     //     },
+    //   },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
     //   },
     // ],
   };
