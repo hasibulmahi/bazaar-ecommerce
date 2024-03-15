5516948a5daf9799255b1ec21e49d62cc43e40e9
Hasibul Mahi <hasibulmahi@gmail.com>
2024-03-15 23:01:01 +0600

    Update index.js

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,18 +1,7 @@
-// import React from "react";
-
-// function Home(props) {
-//   return (
-//     <>
-//       <Layout></Layout>
-//     </>
-//   );
-// }
-
-// export default Home;
 import React from "react";
 
 function Home(props) {
-  return <div></div>;
+  return <>{/* <Layout></Layout> */}</>;
 }
 
 export default Home;
